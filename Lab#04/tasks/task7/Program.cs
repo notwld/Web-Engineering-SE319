024b3a81282800988765ac13ec9e29317ecd5a15
M Waleed <mwfarrukh@gmail.com>
2022-11-20 23:09:01 +0500

    added task#8 and task#9

---
+++ Lab#04/tasks/task7/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[4, 4];
+            int sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    arr[i, j] = i + j;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i == j)
+                    {
+                        sum = sum + arr[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("Sum of diagonals is " + sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab#04/tasks/task8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace task8
+{
+    class Circle
+    {
+        public double radius;
+        public double area;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double Area()
+        {
+            area = 3.14 * radius * radius;
+            return area;
+        }
+    }
+    class Triangle
+    {
+        public double base1;
+        public double height;
+        public double area;
+        public Triangle(double base1, double height)
+        {
+            this.base1 = base1;
+            this.height = height;
+        }
+        public double Area()
+        {
+            area = 0.5 * base1 * height;
+            return area;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(5);
+            Console.WriteLine("The area of the circle is: " + c.Area());
+            Triangle t = new Triangle(5, 6);
+            Console.WriteLine("The area of the triangle is: " + t.Area());
+            Console.ReadLine();
+        }
+    }
+}
